4d38ece1b75e20318d63bbd8cf56359fb84b8f3a
beuri97 <beuri97@gmail.com>
2022-08-10 21:52:18 +1200

    modified server.py

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 from select import select
 from socket import *
 
-soc_1 = socket(AF_UNIX, SOCK_DGRAM)
-soc_2 = socket(AF_UNIX, SOCK_DGRAM)
-soc_3 = socket(AF_UNIX, SOCK_DGRAM)
+soc_1 = socket(AF_INET, SOCK_DGRAM)
+soc_1.bind("",5001)
