0618fc455f93c4422455910990f51725d401a30f
beuri97 <beuri97@gmail.com>
2022-08-17 00:18:38 +1200

    update server.py and client.py

--- client.py
+++ client.py
@@ -1,11 +1,74 @@
 import sys
-from socket import AF_INET, SOCK_DGRAM, gethostbyname_ex, gethostname, socket
+from socket import AF_INET, SOCK_DGRAM, getaddrinfo, socket
 from select import select
 
-HOST = gethostbyname_ex(gethostname())[2][1]
+
+err = '\033[91m'
+norm = '\033[0m'
+len_code = {0x0001, 0x0002, 0x0003}
+
+def dt_response_check(packet: bytearray):
+    """ Check response from server. """
+    if len(packet) != 13:
+        print(f"{err}PACKET ERROR: Packet does not contain 13 bytes.{norm}")
+    
+    elif (packet[0]<<8)|packet[1] != 0x497e:
+        print(f"{err}PACKET ERROR: Packet does not meet key requirement.{norm}")
+    
+    elif (packet[2]<<8)|packet[3] != 0x0002:
+        print(f"{err}PACKET ERROR: Packet does not meet its requirement.{norm}")
+    
+    elif (packet[4]<<8)|packet[5] not in len_code:
+        print(f"{err}PACKET ERROR: Language not support.{norm}")
+    
+    elif (packet[6]<<8)|packet[7] >= 2100:
+        print(f"{err}PACKET ERROR: Packet header contains wrong year info.{norm}")
+    
+    elif packet[8] not in range(1, 13):
+        print(f"{err}PACKET ERROR: Packet header contains wrong month info.{norm}")
+    
+    elif packet[9] not in range(1, 32):
+        print(f"{err}PACKET ERROR: Packet header contains wrong day info.{norm}")
+    
+    elif packet[10] not in range(0, 24):
+        print(f"{err}PACKET ERROR: Packet header contains wrong hour info.{norm}")
+    
+    elif packet[11] not in range(0, 60):
+        print(f"{err}PACKET ERROR: Packet header contains wrong minute info.{norm}")
+    
+    elif packet[12] != (32*3)+8:
+        print(f"{err}PACKET ERROR: There are missing datas in the packetheader.{norm}")
+
+    else:
+        pass
+
+
 soc = socket(AF_INET, SOCK_DGRAM)
-soc.bind((HOST, 7777))
+soc.bind(('', 7777))
+
+dt_request = bytearray(6)
+dt_request[0] = 0x497E >> 8
+dt_request[1] = 0x497E & 0xFF
+dt_request[2] = 0x00
+dt_request[3] = 0x0001
+dt_request[4] = 0x00
+if sys.argv[1].lower() == 'date':
+    dt_request[5] = 0x01
+
+elif sys.argv[1].lower() == 'time':
+    dt_request[5] = 0x02
+
+else:
+    print(f"{err}REQUEST ERROR: Unknown Request Type.{norm}")
 
 
-soc.sendto(b'Hello?', (sys.argv[1], int(sys.argv[2])))
-soc.close()
+soc.sendto(dt_request, (sys.argv[2], int(sys.argv[3])))
+rlist, wlist, time = select([soc], [], [])
+for sock in rlist:
+    msg, addr = sock.recvfrom(50000)
+    print(msg)
+    msg = dt_response_check(msg)
+
+    if msg != None:
+        text = text.decode(encoding='utf-8')
+        print(text)
--- server.py
+++ server.py
@@ -1,15 +1,16 @@
-from calendar import month
 from select import select
 from socket import *
-from datetime import datetime
+from time import localtime, strftime
+import os
 
 
 err = '\033[91m'
 norm = '\033[0m'
 len_code = {0x0001, 0x0002, 0x0003}
 
-def dt_request_check(packet: bytearray):
-    """ check DT-request packet from client """
+def dt_request(packet: bytearray) -> tuple:
+    """ check DT-request packet from client and generate packet to send back to client """
+   
     if len(packet) != 6:
         print(f"{err}ERROR: Packet does not contain 6 bytes.{norm}")
     
@@ -19,48 +20,73 @@ def dt_request_check(packet: bytearray):
     elif (packet[2]<<8)|packet[3] != 0x0001:
         print(f"{err}ERROR: Wrong packet type received.{norm}")
     
-    elif (packet[4]<<8)|packet[5] != 0x0001 or (packet[4]<<8)|packet[5] != 0x0002:
+    elif (packet[4]<<8)|packet[5] >= 2:
         print(f"{err}ERROR: Unknown Request.{norm}")
     
     else:
-        pass
+        print("MESSAGE: Server got package on: ", end = ' ')
+        mg_num = 0x497E
+        pak_type = 0x0002
+        year_now = strftime("%Y", localtime())
+        month_now = strftime("%m", localtime())
+        day_now = strftime("%d", localtime())
+        hour_now = strftime("%H", localtime())
+        min_now = strftime("%M", localtime())
 
-HOST = gethostbyname_ex(gethostname())[2][1]
+        if addr[1] == 5001 and (packet[4]<<8)|packet[5] == 0x0001:          # Date in English
+            l_code = 0x0001
+            pay_load = f"Today's date is {month_now} {day_now}, {year_now}"
+            pay_load = bytes(pay_load, encoding='utf-8')
+        
+        elif addr[1] == 5001 and (packet[4]<<8)|packet[5] == 0x0002:        # Time in English
+            l_code = 0x0001
+            pay_load = f"The current time is {hour_now}:{min_now}"
+            pay_load = bytes(pay_load, encoding='utf-8')
+
+        if addr[1] == 5002 and (packet[4]<<8)|packet[5] == 0x0001:          # Date in Te reo Maori
+            l_code = 0x0002
+       
+        elif addr[1] == 5002 and (packet[4]<<8)|packet[5] == 0x0002:        # Time in Te reo Maori
+            l_code = 0x0002
+
+        if addr[1] == 5003:     
+            l_code = 0x0003
+
+        # DT-Response packet generate begins.
+        # new_packet = bytearray(12)
+        # new_packet[0] = mg_num >> 8
+        # new_packet[1] = mg_num & 0xFF
+        # new_packet[2] = 0x00
+        # new_packet[3] = pak_type
+        # new_packet[4] = 0x00
+        # new_packet[5] = l_code
+        # new_packet[6] = year_now >> 8
+        # new_packet[7] = year_now & 0xFF
+        # new_packet[8] = month_now
+        # new_packet[9] = day_now
+        # new_packet[10] = hour_now
+        # new_packet[11] = min_now
+        # new_packet[12] = len(pay_load)
+        # new_packet += pay_load
+
+        return 
+
+
 soc_1 = socket(AF_INET, SOCK_DGRAM)
-soc_1.bind((HOST,5001))
+soc_1.bind(('',5001))
 soc_2 = socket(AF_INET, SOCK_DGRAM)
-soc_2.bind((HOST,5002))
+soc_2.bind(('',5002))
 soc_3  = socket(AF_INET, SOCK_DGRAM)
-soc_3.bind((HOST,5003))
+soc_3.bind(('',5003))
 
 soc_list = [soc_1, soc_2, soc_3]
 
 while True:
     a, b, c = select(soc_list,[], [])
-    if a[0] not in soc_list:
-        print(f"{err}ERROR: Looks like we got wrong packet.{norm}")
-    else:
+    if a[0] in soc_list:
         msg, addr = a[0].recvfrom(50000)
         print(f"I got {addr} packet")
-        dt_request_check(msg)
-        
-        current = datetime.now()
-
-        mg_num = 0x497E
-        pak_type = 0x0002
-
-        if addr[1] == 5001:
-            l_code = 0x0001
-
-        elif addr[1] == 5002:
-            l_code = 0x0002
-
-        elif addr[1] == 5003:
-            l_code = 0x0003
-
-        year_now = current.strftime("%Y")
-        month_now = current.strftime("%m")
-        day_now = current.strftime("%d")
-        hour_now = current.strftime("%H")
-        min_now = current.strftime("%M")
-        
+        print(msg)
+        new_msg = dt_request(msg)
+        a[0].sendto(new_msg, addr)
+        a[0].close()
